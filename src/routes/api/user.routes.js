f3c514b21c83974d9bc1ae73c0efdff2156b9371
Daniel Antelo <d.anteloguzman@gmail.com>
2024-12-02 20:01:38 +0100

    Rutas con middleware

--- src/routes/api/user.routes.js
+++ src/routes/api/user.routes.js
@@ -1,10 +1,18 @@
 const { getAllUsers, createUser, updateUser, deleteUser } = require('../../controllers/user.controller');
+const { checkToken, checkAdmin, checkRol } = require('../../utils/middleware');
 
 const router = require('express').Router();
 
-router.get('/', getAllUsers);
-router.post('/', createUser);
-router.put('/:userId', updateUser);
-router.delete('/:userId', deleteUser);
+// Ruta para obtener todos los usuarios (solo para administradores)
+router.get('/', checkToken, checkAdmin, getAllUsers);
+
+// Ruta para crear un nuevo usuario (solo para administradores)
+router.post('/', checkToken, checkAdmin, createUser);
+
+// Ruta para actualizar un usuario (disponible para el usuario o administradores)
+router.put('/:userId', checkToken, checkRol('admin'), updateUser);
+
+// Ruta para eliminar un usuario (solo para administradores)
+router.delete('/:userId', checkToken, checkAdmin, deleteUser);
 
 module.exports = router;
